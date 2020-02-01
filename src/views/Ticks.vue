be7ef314d5c6a793837d654df847dcf992e51b23
Daniel Erat <dan@erat.org>
2020-02-01 08:49:11 -0400

    Add crappy code to display ticks in Ticks view.

--- src/views/Ticks.vue
+++ src/views/Ticks.vue
@@ -3,12 +3,88 @@
      found in the LICENSE file. -->
 
 <template>
-  <div>Ticks</div>
+  <v-treeview dense :items="items" :load-children="loadRoute" open-on-click />
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { Location, Route, RouteId } from '@/models';
+import firebase from '@/firebase';
+
+interface Item {
+  id: string; // default 'item-key' property for v-treeview
+  name: string;
+  children?: Item[];
+  routeId?: RouteId;
+}
 
 @Component
-export default class Ticks extends Vue {}
+export default class Ticks extends Vue {
+  items: Item[] = [];
+
+  mounted() {
+    // TODO: Bind the document and compute this dynamically instead.
+    firebase
+      .firestore()
+      .collection('users')
+      .doc('default') // FIXME: user ID
+      .collection('locations')
+      .doc('root')
+      .get()
+      .then(snap => {
+        if (snap.exists) this.items = makeItems(snap.data() as Location);
+      });
+  }
+
+  loadRoute(item: Item) {
+    if (!item.routeId) return;
+
+    return firebase
+      .firestore()
+      .collection('users')
+      .doc('default')
+      .collection('routes')
+      .doc(item.routeId!.toString())
+      .get()
+      .then(snap => {
+        if (!snap.exists) return;
+        const route = snap.data() as Route;
+        item.children = Object.keys(route.ticks).map(id => {
+          const tickId = parseInt(id);
+          const tick = route.ticks[tickId];
+          return {
+            id: `tick-${tickId}`,
+            name: `${tick.date}`,
+          };
+        });
+        console.log(this.items);
+      });
+  }
+}
+
+function makeItems(root: Location, prefix = ''): Item[] {
+  // First, add child locations.
+  const infos: Item[] = Object.keys(root.children)
+    .sort()
+    .map(name => ({
+      id: prefix + name,
+      name: name,
+      children: makeItems(root.children[name], prefix + name + '-'),
+    }));
+
+  // Next, add the routes that are in the location.
+  // TODO: Sort these.
+  Object.keys(root.routes)
+    .map(id => parseInt(id))
+    .forEach(routeId => {
+      infos.push({
+        id: `route-${routeId}`,
+        name: root.routes[routeId],
+        routeId: routeId,
+        children: [], // triggers dynamic loading when item is clicked
+      });
+    });
+
+  return infos;
+}
 </script>
