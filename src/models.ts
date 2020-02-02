ffbc55303350b8d13d0cbce97fd783af626d3fdc
Daniel Erat <dan@erat.org>
2020-02-01 21:07:21 -0400

    Improve comments in models.ts.

--- src/models.ts
+++ src/models.ts
@@ -5,7 +5,7 @@
 export type RouteId = number;
 export type TickId = number;
 
-// A combination of the 'style' and 'leadStyle' fields returned by the get-ticks
+// A combination of the |style| and |leadStyle| fields returned by the get-ticks
 // API endpoint.
 export enum TickStyle {
   UNKNOWN = 0,
@@ -20,7 +20,7 @@ export enum TickStyle {
   LEAD_FELL_HUNG = 9,
 }
 
-// A map value in Route's 'ticks' field.
+// A map value in Route's |ticks| field.
 export interface Tick {
   date: string; // 'YYYY-MM-DD'
   pitches: number;
@@ -30,7 +30,7 @@ export interface Tick {
   grade: string; // optional user-supplied grade, e.g. '5.11a'
 }
 
-// Corresponds to the 'type' field returned by the get-routes API endpoint. In
+// Corresponds to the |type| field returned by the get-routes API endpoint. In
 // addition to a choice between these three types, Mountain Project offers a
 // 'Toprope' checkbox. I'm not bothering with it since it doesn't seem useful in
 // the context of ticks -- the TickStyle enum already describes the user's
@@ -41,7 +41,9 @@ export enum RouteType {
   OTHER = 2,
 }
 
-// A document in the 'routes' subcollection under a user document.
+// A document in the |routes| subcollection under a user document.
+// This contains detailed information about the route itself, along with all of
+// the user's ticks for the route.
 export interface Route {
   name: string;
   type: RouteType;
@@ -52,26 +54,36 @@ export interface Route {
   // maintain a tick count there?
 }
 
-// Partial information about a route stored in Location.
+// Partial information about a route stored in Area.
 export interface RouteSummary {
   name: string;
   grade: string;
 }
-
-export type LocationName = string;
 
 // A unique identifier for an area containing routes. Created by joining the
 // components of the |location| field returned by the get-routes API endpoint
 // with pipes, e.g. 'Colorado|Boulder|Flatirons|South|The Maiden'.
 export type AreaId = string;
 
-// A document in the 'areas' subcollection under a user document.
+// A document in the |areas| subcollection under a user document. Specifically,
+// this is a location (to use Mountain Project's terminology) that includes one
+// or more routes.
+//
+// If a location does not directly include routes but needs to be tracked
+// because it has sublocations with routes, then it's only tracked by AreaMap.
 export interface Area {
-  // Routes that are located in this location (but not in child locations).
+  // Routes that are in this area (but not in child areas).
   routes: Record<RouteId, RouteSummary>;
 }
 
+// The 'map' document in the |areas| subcollection, but also used recursively
+// within that document. This stores the high-level tree of areas. At the top
+// level of the 'map' document, |children| contains top-level locations, e.g.
+// 'Colorado' and 'International'.
 export interface AreaMap {
+  // Areas within this area, keyed by name (e.g. 'Boulder Canyon').
   children: Record<string, AreaMap>;
+  // If the area contains routes, the document ID of the Area document in the
+  // |areas| subcollection describing the area's routes.
   doc?: AreaId;
 }
