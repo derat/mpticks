82e1cb05b6be19be11c5cf052265db770a6bb049
Daniel Erat <dan@erat.org>
2020-02-12 11:42:19 -0400

    Add divider before 'Log out' item in nav drawer.

--- src/App.vue
+++ src/App.vue
@@ -6,19 +6,26 @@
   <v-app>
     <v-navigation-drawer v-model="drawer" app>
       <v-list>
-        <v-list-item
-          v-for="item in navItems"
-          :key="item.to"
-          :to="item.to ? item.to : ''"
-          v-on="item.method ? { click: item.method } : null"
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
+        <template v-for="(item, index) in navItems">
+          <v-divider
+            v-if="item.type == NavItemType.DIVIDER"
+            :key="`divider-${index}`"
+            class="my-2"
+          />
+          <v-list-item
+            v-else-if="item.type == NavItemType.ITEM"
+            :key="item.text"
+            :to="item.to ? item.to : ''"
+            v-on="item.method ? { click: item.method } : null"
+          >
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.text }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
       </v-list>
     </v-navigation-drawer>
 
@@ -40,20 +47,34 @@ import { Component, Vue } from 'vue-property-decorator';
 import firebase from '@/firebase';
 import { routes, When } from '@/router';
 
+enum NavItemType {
+  // A clickable item with text and an icon.
+  ITEM,
+  // A horizontal line.
+  DIVIDER,
+}
+
+// An entry in the navigation drawer.
 interface NavItem {
-  title: string;
-  icon: string;
+  // Type of item to display.
+  type: NavItemType;
+  // Text to display. Only set for ITEM.
+  text?: string;
+  // Material icon name to use. Only set for ITEM.
+  icon?: string;
+  // vue-router route name to navigate to when clicked. Optionally set for ITEM.
   to?: string;
+  // Method on App component to invoke when clicked. Optionally set for ITEM.
   method?: () => void;
+  // When the item should be displayed.
   when?: When;
 }
 
 @Component
 export default class App extends Vue {
-  // Model for navigation drawer.
-  drawer: any = null;
-
-  navItems: NavItem[] = [];
+  drawer: any = null; // model for navigation drawer
+  NavItemType = NavItemType; // hoist for template
+  navItems: NavItem[] = []; // items to display in drawer
 
   mounted() {
     firebase.auth().onAuthStateChanged(() => {
@@ -64,13 +85,30 @@ export default class App extends Vue {
   updateNavItems() {
     const loggedIn = !!firebase.auth().currentUser;
     this.navItems = [
-      { title: 'Log in', icon: 'account_circle', to: 'login' },
-      { title: 'Ticks', icon: 'check', to: 'ticks' },
-      { title: 'Stats', icon: 'assessment', to: 'stats' },
-      { title: 'Import', icon: 'import_export', to: 'import' },
-      { title: 'About', icon: 'info', to: 'about' },
       {
-        title: 'Log out',
+        type: NavItemType.ITEM,
+        text: 'Log in',
+        icon: 'account_circle',
+        to: 'login',
+      },
+      { type: NavItemType.ITEM, text: 'Ticks', icon: 'check', to: 'ticks' },
+      {
+        type: NavItemType.ITEM,
+        text: 'Stats',
+        icon: 'assessment',
+        to: 'stats',
+      },
+      {
+        type: NavItemType.ITEM,
+        text: 'Import',
+        icon: 'import_export',
+        to: 'import',
+      },
+      { type: NavItemType.ITEM, text: 'About', icon: 'info', to: 'about' },
+      { type: NavItemType.DIVIDER },
+      {
+        type: NavItemType.ITEM,
+        text: 'Log out',
         icon: 'exit_to_app',
         method: this.logOut,
         when: When.LOGGED_IN,
