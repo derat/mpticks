56651e7817b162ea0154d725fa75fec92ceee12a
Daniel Erat <dan@erat.org>
2020-02-28 17:35:21 -0400

    Minor improvements to AboutText component.

--- src/components/AboutText.vue
+++ src/components/AboutText.vue
@@ -5,16 +5,16 @@
 <template>
   <div>
     <p>
-      This site lets you import and view your ticks from
+      This app lets you import and view your ticks from
       <a target="_blank" href="https://www.mountainproject.com/"
         >Mountain Project</a
       >.
     </p>
     <p>
       Some users want to tick every climb so they can track their activity or
-      review notes about earlier attempts. At the same time, it can be annoying
-      to see the stats page for a warm-up route cluttered with multiple ticks
-      from the same user.
+      keep notes about earlier attempts. At the same time, it can be annoying to
+      see the stats page for a warm-up route cluttered with multiple ticks from
+      the same user.
       <span v-if="!short"
         >There have been
         <a
@@ -32,21 +32,28 @@
       >
     </p>
     <p>
-      This site provides an alternate location for storing ticks. You submit
-      ticks to Mountain Project like usual using the website or mobile app, but
-      then import them to this site and can delete the original ticks from
-      Mountain Project if you want.
+      This app provides an alternate location for storing ticks. You submit
+      ticks to Mountain Project like usual using the website or mobile app and
+      then import them into this app. After that, you can delete some or all of
+      your ticks from Mountain Project if you want.
     </p>
     <p v-if="!short">
       Mountain Project doesn't currently provide an interface for importing
       ticks, so be sure that you know what you're doing if you choose to delete
-      them from there. Your Mountain Project ticks page has an "Export CSV"
-      link, and you can also <router-link to="/export">export</router-link> all
-      ticks and routes that you import into this site.
+      them from there after importing them here. Your Mountain Project ticks
+      page has an "Export CSV" link, and you can also
+      <router-link to="/export">export</router-link> all ticks and routes that
+      you import into this app.
     </p>
     <p>
-      The source code for this site is available on
-      <a href="https://github.com/derat/mpticks" target="_blank">GitHub</a>.
+      There's
+      <a href="https://github.com/derat/mpticks#mpticks" target="_blank"
+        >more information</a
+      >
+      and source code in the
+      <a href="https://github.com/derat/mpticks" target="_blank"
+        >mpticks GitHub repository</a
+      >.
       <span v-if="!short"
         >If you have questions or comments, don't hesitate to
         <a href="mailto:dan-mpticks@erat.org" target="_blank">email me</a
@@ -54,8 +61,9 @@
       >
     </p>
     <p class="caption">
-      This site doesn't collect any information beyond the email address that
-      you use to log in and the ticks that you import (which Mountain Project
+      This app doesn't collect any information beyond the email address that you
+      use to log in, the number of imports that you've done and the last import
+      time, and the ticks and routes that you import (which Mountain Project
       already makes publically available). I won't give your email address to
       anyone or use it to spam you.
     </p>
